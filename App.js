1f856daeb8f709d1ced3f4c60a2a51bfee1cb931
Shreea Sri <shreeasri@gmail.com>
2023-09-11 21:52:23 +0530

    lesson 3 layning the foundation

--- App.js
+++ App.js
@@ -1,24 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const parent = React.createElement("div", { id: "parent" }, [
-  React.createElement("div", { id: "child" }, [
-    React.createElement("h1", {}, "hello! their"),
-    React.createElement("h2", {}, "hey! their"),
-  ]),
-  React.createElement("div", { id: "child" }, [
-    React.createElement("h1", {}, "hello! their"),
-    React.createElement("h2", {}, "hey! their"),
-  ]),
-]);
-console.log(parent);
+const Title = () => (
+  <h1 className="head" tabIndex="1">
+    Helloo react using JSX
+  </h1>
+);
 
-const heading = React.createElement(
-  "h1",
-  { id: "heading" },
-  "hello world from react"
+//comopnent composition
+const HeadingComponent = () => (
+  <div id="container">
+    <Title />
+    <h1 className="heading"> hey functional component</h1>
+  </div>
 );
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(parent);
+root.render(<HeadingComponent />);
